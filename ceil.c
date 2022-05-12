855b120c2d0a4eb69b87b8f505b3a2e38be6a33c
GrusnyDance <grusny.dance@gmail.com>
2022-05-12 09:09:41 +0200

    ceil done not tested

---
+++ ceil.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+
+long double s21_ceil(double x) {
+  long double res = 0;
+  if (x >= 0)
+    res = (int)x + 1;
+  else
+    res = (int)x;
+  return res;
+}
+
+int main() {
+  double x = -123.3456;
+  long double num = s21_ceil(x);
+  printf("my num is %Lf\n", num);
+  printf("num of standard func is %Lf\n", ceill(x));
+  return 0;
+}
