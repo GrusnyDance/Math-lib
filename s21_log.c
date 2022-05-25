d8e985d97332b595b7c523bfd79846872ab581b4
Elektra Barbrey <elektrab@vo-l5.msk.21-school.ru>
2022-05-25 14:50:37 +0300

    log

--- s21_log.c
+++ s21_log.c
@@ -18,15 +18,21 @@ long double s21_log(double x) {
 }
 
 long double s21_log2(double x) {
-    if (x > -1.00000001 && x < 1.00000001) {
-        int i = 2;
-        x += 1;
-        while (i != 15) {
-            x -= (pow(x, i) / i);
+    if (x > 0.0000000001 && x < 2.0000000001) {
+        --x;
+    
+        int i = 1;
+        long double res = 0;
+        while (i != 10000000) {
+            // res -= (pow(x, i) / i);
+            // i++;
+            // res += (pow(x, i) / i);
+            res += (pow(-1, i - 1) * pow(x, i) / i);
             i++;
-            x += (pow(x, i) / i);
+            
         }
-        return x;
+        // printf("1");
+        return res;
     } else {
         int tmp = 1;
         int n = 0;
@@ -39,12 +45,22 @@ long double s21_log2(double x) {
         }
         tmp /= 2;
         x /= tmp;
-        return s21_log2(2) * n + s21_log2(x);
+        n--;
+        //printf("%lf ", x);
+        return 0.69314718 * n + s21_log2(x);
     }
 }
 
 int main() {
-    printf("%Lf\n", s21_log2(7));
-    printf("%f", log(7));
+    printf("%.12Lf\n", s21_log2(0.0000001));
+    printf("%.12f", log(0.0000001));
+    // int x = 130;
+    // int tmp = 1;
+    // int n = 0;
+    // while (tmp < x) {
+    //     tmp *= 2;
+    //     n++;
+    // }
+    // printf("%d", n);
     return 0;
 }
