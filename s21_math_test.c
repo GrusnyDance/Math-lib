18d5ba7c1b49cc37839d92685c4c21fe2ebc1f44
Spare Lisa <sparelis@vo-e3.msk.21-school.ru>
2022-05-29 16:41:19 +0300

    add tests for acos

--- s21_math_test.c
+++ s21_math_test.c
@@ -1,7 +1,8 @@
 #include "s21_math.h"
-#include <stdlib.h>
+
 #include <check.h>
 #include <math.h>
+#include <stdlib.h>
 #define DIFF pow(10, -7)
 
 // TESTS DONE FOR CEIL
@@ -116,123 +117,113 @@ START_TEST(ceill_10) {
 END_TEST
 
 // TESTS DONE FOR FLOOR
-START_TEST(floorl_1)
-{
-long double num = -145.125555;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_1) {
+  long double num = -145.125555;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
-START_TEST(floorl_2)
-{
-long double num = 874.790261;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_2) {
+  long double num = 874.790261;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
-START_TEST(floorl_3)
-{
-long double num = 599.924044;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_3) {
+  long double num = 599.924044;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
-START_TEST(floorl_4)
-{
-long double num = 923.400213;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_4) {
+  long double num = 923.400213;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
-START_TEST(floorl_5)
-{
-long double num = -412.620530;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_5) {
+  long double num = -412.620530;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
-START_TEST(floorl_6)
-{
-long double num = -913.250907;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_6) {
+  long double num = -913.250907;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
-START_TEST(floorl_7)
-{
-long double num = 992.001280;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_7) {
+  long double num = 992.001280;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
-START_TEST(floorl_8)
-{
-long double num = 565.516067;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_8) {
+  long double num = 565.516067;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
-START_TEST(floorl_9)
-{
-long double num = 628.530256;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_9) {
+  long double num = 628.530256;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
-START_TEST(floorl_10)
-{
-long double num = -291.982749;
-if (isnan((double)(s21_floor(num)))) {
+START_TEST(floorl_10) {
+  long double num = -291.982749;
+  if (isnan((double)(s21_floor(num)))) {
     ck_assert_int_eq((isnan(floorl(num)) != 0), 1);
-} else {
+  } else {
     long double condition = (s21_floor(num) - floorl(num));
     ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
-}
+  }
 }
 END_TEST
 
@@ -347,6 +338,116 @@ START_TEST(abs_10) {
 }
 END_TEST
 
+START_TEST(acos_1) {
+  long double num = -0.652056;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
+START_TEST(acos_2) {
+  long double num = 40.901828;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
+START_TEST(acos_3) {
+  long double num = 37.018012;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
+START_TEST(acos_4) {
+  long double num = -38.276342;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
+START_TEST(acos_5) {
+  long double num = 89.515404;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
+START_TEST(acos_6) {
+  long double num = 85.393896;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
+START_TEST(acos_7) {
+  long double num = 15.215444;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
+START_TEST(acos_8) {
+  long double num = -74.034820;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
+START_TEST(acos_9) {
+  long double num = 96.787762;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
+START_TEST(acos_10) {
+  long double num = -88.079564;
+  if (isnan((double)(s21_acos(num)))) {
+    ck_assert_int_eq((isnan(acos(num)) != 0), 1);
+  } else {
+    long double condition = (s21_acos(num) - acos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+  }
+}
+END_TEST
+
 int main(void) {
   Suite *s1 = suite_create("Core");
   TCase *tc1_1 = tcase_create("Core");
@@ -387,7 +488,17 @@ int main(void) {
   tcase_add_test(tc1_1, abs_8);
   tcase_add_test(tc1_1, abs_9);
   tcase_add_test(tc1_1, abs_10);
-
+  // ACOS
+  tcase_add_test(tc1_1, acos_1);
+  tcase_add_test(tc1_1, acos_2);
+  tcase_add_test(tc1_1, acos_3);
+  tcase_add_test(tc1_1, acos_4);
+  tcase_add_test(tc1_1, acos_5);
+  tcase_add_test(tc1_1, acos_6);
+  tcase_add_test(tc1_1, acos_7);
+  tcase_add_test(tc1_1, acos_8);
+  tcase_add_test(tc1_1, acos_9);
+  tcase_add_test(tc1_1, acos_10);
 
   srunner_run_all(sr, CK_ENV);
   nf = srunner_ntests_failed(sr);
