56669feac6aab98f69a652644f7358564c20cfaa
daniilzastaskov <znata_2014@mail.ru>
2022-05-29 15:01:32 +0300

    lessThanZero

--- s21_log.c
+++ s21_log.c
@@ -3,12 +3,15 @@
 long double s21_log(double x) {
     double alpha = (x - 1) / (x + 1);
     double result = alpha;
-    double save = result * alpha * alpha;
+    if (x <= 0) {
+        result = S21_NAN;
+    } else {
+        double save = result * alpha * alpha;
 
-    for (int i = 2; i <= 1e7; i++) {
-        result += (1.0 / (2 * i - 1)) * save;
-        save = save * alpha * alpha;
+        for (int i = 2; i <= 1e7; i++) {
+            result += (1.0 / (2 * i - 1)) * save;
+            save = save * alpha * alpha;
+        }
     }
-
     return (long double) 2.0 * result;
 }
