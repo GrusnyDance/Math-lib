ff2207ba0682f09f33448ecd6ff8db49bf532e8c
DanilaWhite <60770374+smthinthewayy@users.noreply.github.com>
2022-05-29 16:39:09 +0300

    Добавил тесты для синуса и косинуса

--- s21_math_test.c
+++ s21_math_test.c
@@ -347,6 +347,246 @@ START_TEST(abs_10) {
 }
 END_TEST
 
+START_TEST(sin_1)
+{
+long double num = -36864579.672396;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(sin_2)
+{
+long double num = 417009446.032815;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(sin_3)
+{
+long double num = 677759473.527670;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(sin_4)
+{
+long double num = -896528420.456000;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(sin_5)
+{
+long double num = 46837396.010215;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(sin_6)
+{
+long double num = -803885256.314597;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(sin_7)
+{
+long double num = 1100497120.572486;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(sin_8)
+{
+long double num = 55105461.764664;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(sin_9)
+{
+long double num = -1842504121.289823;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(sin_10)
+{
+long double num = 1033233481.940456;
+if (isnan((double)(s21_sin(num)))) {
+    ck_assert_int_eq((isnan(sin(num)) != 0), 1);
+} else {
+    long double condition = (s21_sin(num) - sin(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_1)
+{
+long double num = -30071291.155215;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_2)
+{
+long double num = -1408190445.698886;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_3)
+{
+long double num = 543179138.816511;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_4)
+{
+long double num = 1211786089.097981;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_5)
+{
+long double num = -1511200530.226901;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_6)
+{
+long double num = 1252688476.467826;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_7)
+{
+long double num = 1935223994.746443;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_8)
+{
+long double num = 1309679703.465514;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_9)
+{
+long double num = -213223855.110455;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
+START_TEST(cos_10)
+{
+long double num = 346667158.578833;
+if (isnan((double)(s21_cos(num)))) {
+    ck_assert_int_eq((isnan(cos(num)) != 0), 1);
+} else {
+    long double condition = (s21_cos(num) - cos(num));
+    ck_assert_int_eq(((condition <= DIFF) && (condition >= -DIFF)), 1);
+}
+}
+END_TEST
+
 int main(void) {
   Suite *s1 = suite_create("Core");
   TCase *tc1_1 = tcase_create("Core");
@@ -387,6 +627,28 @@ int main(void) {
   tcase_add_test(tc1_1, abs_8);
   tcase_add_test(tc1_1, abs_9);
   tcase_add_test(tc1_1, abs_10);
+  // SIN
+  tcase_add_test(tc1_1, sin_1);
+  tcase_add_test(tc1_1, sin_2);
+  tcase_add_test(tc1_1, sin_3);
+  tcase_add_test(tc1_1, sin_4);
+  tcase_add_test(tc1_1, sin_5);
+  tcase_add_test(tc1_1, sin_6);
+  tcase_add_test(tc1_1, sin_7);
+  tcase_add_test(tc1_1, sin_8);
+  tcase_add_test(tc1_1, sin_9);
+  tcase_add_test(tc1_1, sin_10);
+  // COS
+  tcase_add_test(tc1_1, cos_1);
+  tcase_add_test(tc1_1, cos_2);
+  tcase_add_test(tc1_1, cos_3);
+  tcase_add_test(tc1_1, cos_4);
+  tcase_add_test(tc1_1, cos_5);
+  tcase_add_test(tc1_1, cos_6);
+  tcase_add_test(tc1_1, cos_7);
+  tcase_add_test(tc1_1, cos_8);
+  tcase_add_test(tc1_1, cos_9);
+  tcase_add_test(tc1_1, cos_10);
 
 
   srunner_run_all(sr, CK_ENV);
