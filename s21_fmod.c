9bb74b38b5660fd113a96545e88829f89834010e
semiotic <semiotic@student.21-school.ru>
2022-05-29 16:19:06 +0300

    Поменял s21_fmod

--- s21_fmod.c
+++ s21_fmod.c
@@ -1,13 +1,5 @@
 #include "s21_math.h"
 
 long double s21_fmod(double x, double y) {
-    long double res = x;
-    if (y == 0 || x == 0)
-        res = S21_NAN;
-    else
-        while (res > 0)
-            res -= y;
-    if (res < 0)
-        res += y;
-    return (long double) res;
+  return x - ((x / y) < 0 ? s21_ceil(x / y) : s21_floor(x / y)) * y;
 }
