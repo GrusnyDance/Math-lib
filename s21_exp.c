e640fefa1edfe32e96cf9298ee1ad5a205cda25d
semiotic <semiotic@student.21-school.ru>
2022-06-01 19:33:22 +0300

    Короче оно почти работает, там прям 1-2 знака неточность при больших числах и я хз что с этим делать

--- s21_exp.c
+++ s21_exp.c
@@ -1,14 +1,13 @@
 #include "s21_math.h"
 
 long double s21_exp(double x) {
-  int sign = x < 0;
-  x = x < 0 ? -x : x;
-  long double res = 0;
-  long long i = 1;
-  long double raw = 1;
-  while (s21_fabs(raw) > 1e-200) {
-    res += raw;
-    raw = (raw * x) / i++;
-  }
-  return sign ? 1 / res : res;
+    long double result = 1;
+    long double temp = x;
+    unsigned flag = 1;
+    while (flag < 1000) {
+        result += temp;
+        flag += 1;
+        temp *= (x / flag);
+    }
+    return result;
 }
