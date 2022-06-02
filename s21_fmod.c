b082b49bafed40a46cf349a822c738ef12761107
GrusnyDance <grusny.dance@gmail.com>
2022-06-02 20:31:11 +0200

    fix division by zero

--- s21_fmod.c
+++ s21_fmod.c
@@ -1,5 +1,10 @@
 #include "s21_math.h"
 
 long double s21_fmod(double x, double y) {
-  return x - ((x / y) < 0 ? s21_ceil(x / y) : s21_floor(x / y)) * y;
+  long double res;
+  if (y == 0)
+    res = S21_NAN;
+  else
+    res = x - ((x / y) < 0 ? s21_ceil(x / y) : s21_floor(x / y)) * y;
+  return res;
 }
