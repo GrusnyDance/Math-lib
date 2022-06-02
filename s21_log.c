0a3de2fd247ddbb2689ce0b50c2831f7d329606b
semiotic <semiotic@student.21-school.ru>
2022-06-02 11:41:56 +0300

    ЦАРЬ-ЛОГАРИФМ

--- s21_log.c
+++ s21_log.c
@@ -3,7 +3,7 @@
 long double s21_taylor_log(double x) {
     long double result = x, member = x;
     for (int i = 2; s21_fabs(member) > S21_EPS; i++) {
-        member *= -1 * (i - 1) * (x / i);
+        member *= -((i - 1) * (x / i));
         result += member;
     }
     return result;
@@ -14,17 +14,12 @@ long double s21_log(double x) {
     if (x <= 0.0) {
         result = S21_NAN;
     } else {
-        int first_part = 0;
-        while ((int) x >= 10) {
-            first_part++;
-            x /= 10;
-        }
-        int second_part = 0;
+        int accuracy = 0;
         while ((int) x >= 2) {
-            second_part++;
+            accuracy++;
             x /= 2;
         }
-        result = s21_taylor_log(x - 1) + first_part * S21_LN10 + second_part * S21_LN2;
+        result = accuracy * S21_LN2 + s21_taylor_log(x - 1);
     }
     return result;
 }
